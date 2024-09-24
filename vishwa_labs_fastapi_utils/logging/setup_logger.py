3b18709efc6252af9830e6fb90571b49edc6467e
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2024-09-24 18:20:20 +0530

    feat: Enable Otel + rebrand SDK

--- vishwa_labs_fastapi_utils/logging/setup_logger.py
+++ vishwa_labs_fastapi_utils/logging/setup_logger.py
@@ -1,6 +1,6 @@
 import logging
 from logging.config import dictConfig
 
-from xpuls_fastapi_utils.logging.fastapi_log_config import FALogConfig
+from vishwa_labs_fastapi_utils.logging.fastapi_log_config import FALogConfig
 dictConfig(FALogConfig().dict())
 logger = logging.getLogger("default")
