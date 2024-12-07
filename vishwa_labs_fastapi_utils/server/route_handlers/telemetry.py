97b31014c547a94160fa1dd1cdbfb1736fca5efd
Sai Sharan Tangeda <31337767+SHARANTANGEDA@users.noreply.github.com>
2024-12-08 01:11:18 +0530

    Update telemetry.py

--- vishwa_labs_fastapi_utils/server/route_handlers/telemetry.py
+++ vishwa_labs_fastapi_utils/server/route_handlers/telemetry.py
@@ -5,6 +5,7 @@ class TelemetryAPI:
     def __init__(self) -> None:
         self.router = APIRouter()
         self.router.add_api_route("/healthcheck", self.healthcheck_endpoint, methods=["GET"])
+        self.router.add_api_route("/ping", self.healthcheck_endpoint, methods=["GET"])
 
     async def healthcheck_endpoint(self):
         return "!pong"
